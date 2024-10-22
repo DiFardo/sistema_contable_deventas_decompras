3df253095ce750f4ad66f27af9208d7c0903c605
DiFardo <eduardofabioelmaspro@gmail.com>
2024-10-22 10:13:12 -0500

    cambios(menu_rutas)
    aca esta todo el menu navbar y los modulos

--- maestra_iniciar.py
+++ maestra_iniciar.py
@@ -1,11 +1,9 @@
-from flask import Flask, flash, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect, make_response, flash
 import hashlib
 from flask_jwt_extended import JWTManager, create_access_token
 import controladores.controlador_usuarios as controlador_usuarios
-import clases.clase_usuario as clase_usuario
-from bd_conexion import obtener_conexion  # Asegúrate de que la conexión a la base de datos esté configurada correctamente
-from controladores.controlador_cuentas import obtener_todas_cuentas  # Importa la función para obtener las cuentas
-from flask import g
+from bd_conexion import obtener_conexion
+from controladores.controlador_cuentas import obtener_todas_cuentas
 
 app = Flask(__name__)
 app.debug = True
@@ -31,7 +29,7 @@ def login():
     if not token:
         return render_template("login_user.html")
     if validar_token():
-        return redirect("/index")  # Cambiado para redirigir a index.html
+        return redirect("/index")
     return render_template("login_user.html")
 
 
@@ -39,62 +37,73 @@ def login():
 def index():
     token = request.cookies.get('token')
     dni = request.cookies.get('dni')
-    usuario = controlador_usuarios.obtener_usuario(dni)  # Obtener el usuario con DNI desde la base de datos
+    usuario = controlador_usuarios.obtener_usuario(dni)
 
     breadcrumbs = [
         {'name': 'Inicio', 'url': '/index'}
     ]
-    return render_template("index.html", breadcrumbs=breadcrumbs, usuario=usuario)  # Pasar el usuario a la plantilla
+    return render_template("index.html", breadcrumbs=breadcrumbs, usuario=usuario)
 
 
-
-@app.errorhandler(404)
-def page404(e):
+@app.route("/cuentas")
+def cuentas():
+    cuentas_data = obtener_todas_cuentas()
     token = request.cookies.get('token')
     dni = request.cookies.get('dni')
-    usuario = controlador_usuarios.obtener_usuario(dni)  # Obtener el usuario con DNI desde la base de datos
+    usuario = controlador_usuarios.obtener_usuario(dni)
 
     breadcrumbs = [
-        {'name': 'Inicio', 'url': '/'},
-        {'name': 'Página No Encontrada', 'url': '#'}
+        {'name': 'Inicio', 'url': '/index'},
+        {'name': 'Cuentas contables', 'url': '/cuentas'}
     ]
-    return render_template('page404.html', breadcrumbs=breadcrumbs, usuario=usuario), 404  # Pasar el usuario a la plantilla
+    return render_template("cuentas.html", cuentas=cuentas_data, breadcrumbs=breadcrumbs, usuario=usuario)
 
 
 
-# INICIAR SESION
-@app.route("/procesar_login", methods=["POST"])
-def procesar_login():
-    try:
-        dni = request.form["dni"]
-        password = request.form["password"]
-        print(f"Intentando iniciar sesión con DNI: {dni}")
-        usuario = controlador_usuarios.obtener_usuario(dni)
-        if not usuario:
-            print("Usuario no encontrado")
-            flash("Usuario no encontrado.")
-            return redirect("/login_user")
-        h = hashlib.new('sha256')
-        h.update(bytes(password, encoding="utf-8"))
-        encpass = h.hexdigest()
-        print(f"Contraseña encriptada ingresada: {encpass}, Contraseña esperada: {usuario[2]}")
-        if encpass == usuario[2]:  # Compara con el campo pass
-            access_token = create_access_token(identity=dni)
-            controlador_usuarios.actualizartoken_usuario(dni, access_token)  # Actualiza el token en la base de datos
-            resp = make_response(redirect("/index"))  # Cambiado para redirigir a index.html
-            resp.set_cookie('token', access_token)
-            resp.set_cookie('dni', dni)
-            print("Inicio de sesión exitoso")
-            return resp
-        else:
-            print("Contraseña incorrecta")
-            flash("Contraseña incorrecta.")
-            return redirect("/login_user")
-    except Exception as e:
-        print(f"Error en el inicio de sesión: {e}")
-        flash("Ocurrió un error. Por favor, inténtelo de nuevo.")
-        return redirect("/login_user")
 
+@app.route("/libro_caja")
+def libro_caja():
+    token = request.cookies.get('token')
+    dni = request.cookies.get('dni')
+    usuario = controlador_usuarios.obtener_usuario(dni)
+
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': '/index'},
+        {'name': 'Libro Caja y Bancos', 'url': '/libro_caja'}
+    ]
+    movimientos = []
+
+    return render_template("libro_caja.html", movimientos=movimientos, breadcrumbs=breadcrumbs, usuario=usuario)
+
+
+@app.route("/libro_diario")
+def libro_diario():
+    token = request.cookies.get('token')
+    dni = request.cookies.get('dni')
+    usuario = controlador_usuarios.obtener_usuario(dni)
+
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': '/index'},
+        {'name': 'Libro Diario', 'url': '/libro_diario'}
+    ]
+    movimientos = []
+
+    return render_template("libro_diario.html", movimientos=movimientos, breadcrumbs=breadcrumbs, usuario=usuario)
+
+
+@app.route("/libro_mayor")
+def libro_mayor():
+    token = request.cookies.get('token')
+    dni = request.cookies.get('dni')
+    usuario = controlador_usuarios.obtener_usuario(dni)
+
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': '/index'},
+        {'name': 'Libro Mayor', 'url': '/libro_mayor'}
+    ]
+    movimientos = []
+
+    return render_template("libro_mayor.html", movimientos=movimientos, breadcrumbs=breadcrumbs, usuario=usuario)
 
 @app.route("/procesar_logout")
 def procesar_logout():
@@ -111,40 +120,6 @@ def procesar_logout():
         flash("Error al cerrar la sesión.")
         return redirect("/login_user")
 
-
-@app.route("/cuentas")
-def cuentas():
-    cuentas_data = obtener_todas_cuentas()  # Llama a la función para obtener los datos de las cuentas
-    token = request.cookies.get('token')
-    dni = request.cookies.get('dni')
-    usuario = controlador_usuarios.obtener_usuario(dni)  # Obtener el usuario con DNI desde la base de datos
-
-    breadcrumbs = [
-        {'name': 'Inicio', 'url': '/index'},
-        {'name': 'Cuentas contables', 'url': '/cuentas'}
-    ]
-    return render_template("cuentas.html", cuentas=cuentas_data, breadcrumbs=breadcrumbs, usuario=usuario)  # Pasar el usuario a la plantilla
-
-from flask import g
-
-
-#@app.before_request
-#def cargar_usuario():
-#   token = request.cookies.get('token')
- #   dni = request.cookies.get('dni')
- #   if dni:
-  #      g.usuario = controlador_usuarios.obtener_usuario(dni)  # Almacena el usuario en g
-   # else:
-    #    g.usuario = None  # Si no hay dni, asegura que g.usuario sea None
-
-
-#@app.context_processor
-#def contexto_global():
- #   return {'usuario': getattr(g, 'usuario', None)}  # Devuelve el usuario o None si no está definido
-
-
-
-
-# Iniciar el servidor
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
+
