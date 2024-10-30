44a09a4585da779463c898628eb0a73b5a8a82f2
Cristian0473 <117053265+Cristian0473@users.noreply.github.com>
2024-10-30 09:48:58 -0500

    olamundo

--- bd_conexion.py
+++ bd_conexion.py
@@ -1,11 +1,11 @@
-from decouple import config 
+from decouple import config
 import psycopg2
 
 def obtener_conexion():
     return psycopg2.connect(
-        host=config('POSTGRES_HOST'),
-        port=config('POSTGRES_PORT'),
-        user=config('POSTGRES_USER'),
-        password=config('POSTGRES_PASSWORD'),
-        database=config('POSTGRES_DB')
+        host=config('POSTGRES_HOST', default='pg-servercontable-calidad-2024.d.aivencloud.com'),
+        port=config('POSTGRES_PORT', default='26134'),
+        user=config('POSTGRES_USER', default='avnadmin'),
+        password=config('POSTGRES_PASSWORD', default='AVNS_8q6hd4Rhe2XMjRIpU_G'),
+        database=config('POSTGRES_DB', default='prueba_perfil')
     )
--- maestra_iniciar.py
+++ maestra_iniciar.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, make_response, flash, g, jsonify
+from flask import Flask, render_template, request, redirect, make_response, flash, g, jsonify, url_for
+import os
 import hashlib
 from flask_jwt_extended import JWTManager, create_access_token
 import controladores.controlador_usuarios as controlador_usuarios
@@ -6,13 +7,99 @@ import controladores.controlador_ventas as controlador_ventas
 import clases.clase_usuario as clase_usuario
 from bd_conexion import obtener_conexion  # Asegúrate de que la conexión a la base de datos esté configurada correctamente
 from controladores.controlador_cuentas import obtener_todas_cuentas, obtener_cuentas_por_categoria_endpoint, añadir_cuenta
+from werkzeug.utils import secure_filename
+
+# Directorio donde se guardarán las imágenes de perfil
+UPLOAD_FOLDER = 'static/img/perfiles'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
 app.debug = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['SESSION_COOKIE_SECURE'] = True
 #app.run(ssl_context=('cert.pem', 'key.pem'))
 
+# Verificar si el archivo es una imagen permitida
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/subir_imagen_perfil", methods=["POST"])
+def subir_imagen_perfil():
+    # Verificar si el archivo ha sido enviado en la solicitud
+    if 'imagen_perfil' not in request.files:
+        flash('No se ha seleccionado ningún archivo.')
+        return redirect(url_for('perfil_usuario'))
+
+    file = request.files['imagen_perfil']
+
+    # Verificar si no se ha seleccionado ningún archivo
+    if file.filename == '':
+        flash('No se seleccionó ningún archivo.')
+        return redirect(url_for('perfil_usuario'))
+
+    # Verificar si el archivo tiene un formato permitido
+    if file and allowed_file(file.filename):
+        # Aseguramos que el nombre del archivo sea seguro
+        filename = secure_filename(file.filename)
+        # Guardamos el archivo en el directorio de imágenes de perfiles
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Actualizar la tabla personas con la imagen
+        dni = request.cookies.get('dni')
+        controlador_usuarios.actualizar_imagen_perfil(dni, filename)
+
+        flash('Imagen de perfil actualizada con éxito.')
+        return redirect(url_for('perfil_usuario'))
+
+    # Si el tipo de archivo no es permitido
+    flash('Tipo de archivo no permitido. Selecciona una imagen válida (png, jpg, jpeg, gif).')
+    return redirect(url_for('perfil_usuario'))
+
+
+
+
+def obtener_descripcion_rol(rol):
+    descripciones = {
+        "Coordinador general": "Responsable de supervisar y organizar el equipo para cumplir objetivos, asegurando una comunicación efectiva y la resolución de problemas. Facilita la toma de decisiones, gestiona riesgos y mantiene informadas a las partes interesadas sobre el progreso del proyecto.",
+        "Administrador de base de datos": "Responsable del diseño, implementación, seguridad y mantenimiento de la base de datos. Debe tener experiencia en la optimización del rendimiento de consultas y asegurar la integridad de los datos, con un enfoque en la resolución de problemas y la gestión eficiente de los datos.",
+        "Analista": "Encargado de recopilar y analizar los requisitos del sistema. Debe ser capaz de identificar las necesidades del cliente y traducirlas en especificaciones técnicas claras para el equipo. Fuerte capacidad de análisis y comunicación efectiva son clave.",
+        "Diseñador": "Encargado de la creación del diseño visual y de la experiencia del usuario (UI/UX). Debe ser capaz de crear interfaces atractivas y funcionales, asegurando que el sistema sea intuitivo y fácil de usar para los usuarios finales.",
+        "Arquitecto de software": "Responsable de diseñar la estructura técnica del sistema, seleccionando tecnologías y definiendo los componentes clave. Debe tener una visión amplia del sistema y asegurarse de que el software cumpla con los requisitos de escalabilidad, seguridad y eficiencia.",
+        "Programador": "Encargados de la codificación del sistema siguiendo las especificaciones del analista y el diseño del arquitecto. Deben tener experiencia en lenguajes de programación adecuados y ser capaces de trabajar en equipo, respetando plazos y estándares de calidad.",
+        "Supervisor de calidad": "Responsable de asegurar que el sistema cumpla con los estándares de calidad definidos. Debe gestionar las pruebas y asegurar que se mantengan altos niveles de rendimiento, usabilidad y seguridad, monitoreando el progreso y haciendo ajustes si es necesario.",
+        "Tester": "Encargados de realizar pruebas funcionales y de rendimiento del sistema para identificar errores y áreas de mejora. Deben tener habilidades técnicas para diseñar casos de prueba efectivos y capacidad para detectar problemas antes del despliegue del sistema.",
+        "Capacitador": "Responsable de desarrollar y ejecutar planes de capacitación para los usuarios finales. Debe ser capaz de crear manuales y ofrecer formación clara y efectiva, asegurándose de que los usuarios puedan manejar el sistema correctamente.",
+        "Asesor": "Ofrece asesoramiento especializado en áreas clave del proyecto, como estrategias de negocio, tecnología o gestión, y guía al equipo en la toma de decisiones críticas para el éxito del proyecto."
+    }
+    return descripciones.get(rol, "Rol no identificado")
+
+
+
+
+
+# Ruta para eliminar la imagen de perfil
+@app.route("/eliminar_imagen_perfil", methods=["POST"])
+def eliminar_imagen_perfil():
+    dni = request.cookies.get('dni')
+    
+    # Verificar si el usuario tiene una imagen
+    imagen_actual = controlador_usuarios.obtener_imagen_perfil(dni)
+    
+    if not imagen_actual:
+        flash('No tienes una imagen de perfil para eliminar.')
+        return redirect(url_for('perfil_usuario'))
+
+    # Eliminar la imagen del servidor
+    if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], imagen_actual)):
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], imagen_actual))
+
+    # Eliminar la referencia de la imagen en la base de datos
+    controlador_usuarios.eliminar_imagen_perfil(dni)
+
+    flash('Imagen de perfil eliminada con éxito.')
+    return redirect(url_for('perfil_usuario'))
 
 
 # Inicializa JWTManager
@@ -44,11 +131,28 @@ def index():
     token = request.cookies.get('token')
     dni = request.cookies.get('dni')
     usuario = controlador_usuarios.obtener_usuario(dni)
+    
+    if usuario and (not usuario[6] or usuario[6] is None):
+        usuario = list(usuario)  
+        usuario[6] = "perfil_defecto.png" 
 
     breadcrumbs = [
         {'name': 'Inicio', 'url': '/index'}
     ]
     return render_template("index.html", breadcrumbs=breadcrumbs, usuario=usuario)
+
+@app.route("/actualizar_perfil", methods=["POST"])
+def actualizar_perfil():
+    dni = request.cookies.get('dni')
+    nombres = request.form['nombres']
+    apellidos = request.form['apellidos']
+    
+    # Actualiza los datos
+    controlador_usuarios.actualizar_perfil_usuario(dni, nombres, apellidos)
+    
+    flash("Perfil actualizado correctamente.")
+    return redirect(url_for('perfil_usuario'))
+
 
 @app.route("/libro_caja")
 def libro_caja():
@@ -136,20 +240,24 @@ def productos():
 def procesar_login():
     try:
         dni = request.form["dni"]
-        password = request.form["password"]
+        password = request.form["password"].strip()  # Elimina espacios en blanco adicionales
         print(f"Intentando iniciar sesión con DNI: {dni}")
+        
+        # Obtenemos el usuario
         usuario = controlador_usuarios.obtener_usuario(dni)
         if not usuario:
             print("Usuario no encontrado")
             flash("Usuario no encontrado.")
             return redirect("/login_user")
 
+        # Encriptamos la contraseña ingresada por el usuario
         h = hashlib.new("sha256")
-        h.update(bytes(password, encoding="utf-8"))
-        encpass = h.hexdigest()
+        h.update(password.encode('utf-8'))  # Asegúrate de codificar la contraseña
+        encpass = h.hexdigest().lower()  # Convertimos el hash a minúsculas para la comparación
         print(f"Contraseña encriptada ingresada: {encpass}, Contraseña esperada: {usuario[2]}")
 
-        if encpass == usuario[2]:  # Compara con el campo pass
+        # Comparamos el hash de la contraseña ingresada con el hash almacenado
+        if encpass == usuario[2].lower():  # Convertimos ambos a minúsculas para evitar problemas de mayúsculas/minúsculas
             access_token = create_access_token(identity=dni)
             controlador_usuarios.actualizartoken_usuario(dni, access_token)  # Actualiza el token en la base de datos
             resp = make_response(redirect("/index"))  # Cambiado para redirigir a index.html
@@ -243,6 +351,31 @@ def cuentas_añadir():
         return añadir_cuenta()
     except Exception as e:
         return jsonify({'error': f'Error en el servidor: {str(e)}'}), 500
+
+@app.route("/perfil_usuario")
+def perfil_usuario():
+    token = request.cookies.get('token')
+    dni = request.cookies.get('dni')
+
+    if not token or not validar_token():
+        return redirect("/login_user")
+    
+    usuario = controlador_usuarios.obtener_usuario(dni)
+    perfil = controlador_usuarios.obtener_detalles_perfil(dni)
+    
+    if not perfil:
+        flash("Usuario no encontrado.")
+        return redirect("/index")
+    
+    # Obtener la descripción del rol
+    descripcion_rol = obtener_descripcion_rol(perfil[3])
+
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': '/index'},
+        {'name': 'Perfil del Usuario', 'url': '/perfil_usuario'}
+    ]
+    
+    return render_template("perfil_usuario.html", usuario=usuario, perfil=perfil, descripcion_rol=descripcion_rol, breadcrumbs=breadcrumbs)
 
 # Iniciar el servidor
 
